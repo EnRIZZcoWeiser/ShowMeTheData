5f83f485ba1956375935446bc9494f8a347b46c8
EnRIZZcoWeiser <enrycoweiser@outlook.it>
2026-03-21 17:03:48 +0100

    project creation

---
+++ src/main/java/org/enrycoweiser/showmethedata/ShowMeTheDataApplication.java
@@ -0,0 +1,13 @@
+package org.enrycoweiser.showmethedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeTheDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowMeTheDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/enrycoweiser/showmethedata/ShowMeTheDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enrycoweiser.showmethedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeTheDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
